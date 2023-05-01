ece36d8dbf068e494fb770771a120ee481f46446
Robert Nicolescu <robertnicolescu42@yahoo.com>
2023-05-01 17:36:34 +0300

    Added ram option

--- script.py
+++ script.py
@@ -11,7 +11,10 @@ from sklearn.preprocessing import MinMaxScaler
 import csv
 
 # load the training data
-data = pd.read_csv('tracks.csv', nrows=200000)
+data = pd.read_csv('tracks.csv')
+
+# if your computer doesn't have enough RAM, comment out the above line and uncomment the below line to load the entire dataset
+# data = pd.read_csv('tracks.csv', nrows=200000)
 orig_data = data.to_numpy()
 
 # drop irrelevant columns
